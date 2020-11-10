2ebdb34462776c122448f04749b4fd2f9eb3e5b7
robertlin717 <72426746+robertlin717@users.noreply.github.com>
2020-11-10 14:30:52 +0800

    new website upload

---
+++ test.js
@@ -0,0 +1,36 @@
+function website1(){
+  window.location= "index.html";
+}
+function website2(){
+  window.location = "death.html";
+}
+function website3(){
+  window.location = "love.html";
+}
+function website4(){
+  window.location = "concept.html";
+}
+function photo1(){
+  window.location ="photo1.html";
+}
+function photo2(){
+  window.location ="photo2.html";
+}
+function photo3(){
+  window.location ="photo3.html";
+}
+function photo4(){
+  window.location ="photo4.html";
+}
+function photo5(){
+  window.location ="photo5.html";
+}
+function photo6(){
+  window.location ="photo6.html";
+}
+function photo7(){
+  window.location ="photo7.html";
+}
+function photo8(){
+  window.location ="photo8.html";
+}
